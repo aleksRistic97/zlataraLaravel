cd362374dbe655db853d94741cb26b665663f04f
aleksRistic97 <ar20180420@student.fon.bg.ac.rs>
2022-01-12 21:29:02 +0100

    kreirani factory za modele

--- zlataraApp/database/factories/NakitFactory.php
+++ zlataraApp/database/factories/NakitFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Kategorija;
 
 class NakitFactory extends Factory
 {
@@ -13,8 +14,17 @@ class NakitFactory extends Factory
      */
     public function definition()
     {
+        
         return [
-            //
+            'naziv' => $this->faker->randomElement($array = array ('srebrna ogrlica','zlatna narukvica','dijamantska ogrlica','verenicki prsten','bff narukvice')),
+            'opis' => $this->faker->sentence($nbWords = 6, $variableNbWords = true),
+            'boja_zlata' => $this->faker->randomElement($array = array ('zuta','bela','roze','zelena')),
+            'proizvedeno_u' => $this->faker->country(),
+           
+          
+            'cena' =>  $this->faker->numberBetween($min =1000, $max = 10000),
+            'kategorija_id' => Kategorija::find(random_int(1,Kategorija::count()))
+
         ];
     }
 }
