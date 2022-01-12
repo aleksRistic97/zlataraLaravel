20121f0db5fc4ef26d71e0111402ad28a5b93d3c
aleksRistic97 <ar20180420@student.fon.bg.ac.rs>
2022-01-12 22:09:01 +0100

    kreirani kontroleri za api rute

---
+++ zlataraApp/app/Http/Controllers/NakitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nakit;
+use Illuminate\Http\Request;
+
+class NakitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Nakit  $nakit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Nakit $nakit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Nakit  $nakit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Nakit $nakit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Nakit  $nakit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Nakit $nakit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Nakit  $nakit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Nakit $nakit)
+    {
+        //
+    }
+}
---
+++ zlataraApp/app/Http/Controllers/PorudzbenicaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Porudzbenica;
+use Illuminate\Http\Request;
+
+class PorudzbenicaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Porudzbenica  $porudzbenica
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Porudzbenica $porudzbenica)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Porudzbenica  $porudzbenica
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Porudzbenica $porudzbenica)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Porudzbenica  $porudzbenica
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Porudzbenica $porudzbenica)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Porudzbenica  $porudzbenica
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Porudzbenica $porudzbenica)
+    {
+        //
+    }
+}
